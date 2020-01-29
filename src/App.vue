4fffe2bbd5216d3f5a0e716d467da0623bafd02b
sayuriw <sayuri.wichmann@gmail.com>
2020-01-29 15:03:20 +0100

    first components added

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,60 @@
 <template>
   <div id="app" class="container mt-5">
     <h1>My Shop</h1>
-    <p class="animated fadeInRight">Take a look at our offerings</p>
-    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+    <price-slider
+      :sliderStatus="sliderStatus"
+      :maximum.sync="maximum"
+    ></price-slider>
+    <product-list
+      :products="products"
+      :maximum="maximum"
+      @add="addItem"
+    ></product-list>
   </div>
 </template>
 
 <script>
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
-
+import ProductList from './components/ProductList.vue'
+import PriceSlider from './components/PriceSlider.vue'
 export default {
   name: 'app',
+  data: function() {
+    return {
+      maximum: 99,
+      sliderStatus: true,
+      cart: [],
+      products: null
+    }
+  },
+  methods: {
+    addItem: function(product) {
+      var whichProduct
+      var existing = this.cart.filter(function(item, index) {
+        if (item.product.id == Number(product.id)) {
+          whichProduct = index
+          return true
+        } else {
+          return false
+        }
+      })
+
+      if (existing.length) {
+        this.cart[whichProduct].qty++
+      } else {
+        this.cart.push({ product: product, qty: 1 })
+      }
+    }
+  },
   components: {
-    FontAwesomeIcon
+    ProductList,
+    PriceSlider
+  },
+  mounted: function() {
+    fetch('https://hplussport.com/api/products/order/price')
+      .then(response => response.json())
+      .then(data => {
+        this.products = data
+      })
   }
 }
 </script>
-
-
--- src/main.js
+++ src/main.js
@@ -1,20 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import "bootstrap"
-import {library} from "@fortawesome/fontawesome-svg-core"
+import 'bootstrap'
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-import "bootstrap/dist/css/bootstrap.css"
-import "animate.css/animate.css"
+import 'bootstrap/dist/css/bootstrap.css'
+import 'animate.css/animate.css'
 
-import {
-  faShoppingCart,
-  faDollarSign
-} from "@fortawesome/free-solid-svg-icons"
+import { faShoppingCart, faDollarSign } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faShoppingCart, faDollarSign)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
